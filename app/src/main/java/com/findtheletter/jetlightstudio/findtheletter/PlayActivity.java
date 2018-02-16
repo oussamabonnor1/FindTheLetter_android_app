7d2c531f6bc522ee0a871e007649ff374a0ea6d6
Oussama BONNOR <oussamabonnor@gmail.com>
2018-02-16 14:44:57 +0100

    Initial commit

---
+++ app/src/main/java/com/findtheletter/jetlightstudio/findtheletter/PlayActivity.java
@@ -0,0 +1,13 @@
+package com.findtheletter.jetlightstudio.findtheletter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+}
