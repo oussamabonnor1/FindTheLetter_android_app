fb290f2d95255b6ae67c6546f873e07181505ea1
Oussama BONNOR <oussamabonnor@gmail.com>
2018-02-16 23:55:00 +0100

    image play activity fully operational

--- app/src/main/java/com/findtheletter/jetlightstudio/findtheletter/ImageActivity.java
+++ app/src/main/java/com/findtheletter/jetlightstudio/findtheletter/ImageActivity.java
@@ -3,10 +3,12 @@ package com.findtheletter.jetlightstudio.findtheletter;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -16,8 +18,7 @@ public class ImageActivity extends AppCompatActivity {
     EditText textField;
     ProgressBar progressBar;
     ArrayList<Character> characters = new ArrayList<>();
-    String words[] = {"castle", "giraffe", "saturn", "eagle", "africa", "radio", "alarm", "pizza", "money", "sheep", "rain", "penguin", "television", "tree", "fire"};
-    Word jetlight;
+    String words[] = {"castle", "giraffe", "planet", "eagle", "africa", "radio", "alarm", "pizza", "money", "sheep", "rain", "penguin", "television", "tree", "fire"};
     int index = 0;
     int score = 0;
 
@@ -29,7 +30,6 @@ public class ImageActivity extends AppCompatActivity {
         scoreText = (TextView) findViewById(R.id.scoreText);
         textField = (EditText) findViewById(R.id.textField);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        jetlight = new Word(1, score);
         setImage();
     }
 
@@ -39,5 +39,17 @@ public class ImageActivity extends AppCompatActivity {
         index++;
     }
 
-
+    public void checkImage(View v){
+        String msg;
+        if (textField.getText().toString().equals(words[index-1])) {
+            msg = "Correct!";
+            score += 15;
+            float progress = (float) index / words.length;
+            progressBar.setProgress((int) (progress * 100));
+            scoreText.setText("Score: " + String.format("%02d", score));
+            setImage();
+        } else msg = "try again...";
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+        textField.setText("");
+    }
 }
